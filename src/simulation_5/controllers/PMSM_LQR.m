a9d743636d5177b72c0bed6440f3446da14fa51d
nicobotol <niccolo.andreetta@gmail.com>
2023-07-14 16:21:16 +0200

    Sending revison to professors

--- src/simulation_5/controllers/PMSM_LQR.m
+++ src/simulation_5/controllers/PMSM_LQR.m
@@ -14,6 +14,7 @@ A = [ -Rs/L Lambda*p/L 0;
 B = [-1/L 0 0]';% 0 0 0; 0 0 0]';
 E = [0 1/J 0]';
 C = [1 0 0];
+D = [0];
 
 K = lqr(A,B,eye(3),1);
 S = eye(3);
@@ -21,13 +22,26 @@ R = 1;
 Sf = eye(3);
 dt  = 1e-3;
 
-stop_time = 3;
-time = dt:dt:stop_time;
-states_len = 3;
-inputs_len = 1;
-K = lqr_gain(A, B, S, R, stop_time/dt, Sf, states_len, inputs_len, dt);
-for i = 1:length(time)
-  K_gain(:,i) = K(:,:, i);
-end
+% stop_time = 3;
+% time = dt:dt:stop_time;
+% states_len = 3;
+% inputs_len = 1;
+% K = lqr_gain(A, B, S, R, stop_time/dt, Sf, states_len, inputs_len, dt);
+% for i = 1:length(time)
+%   K_gain(:,i) = K(:,:, i);
+% end
+omega_R0 = 0;
+stop_time = simulation.stop_time;
+Q = 100*[1 0 0;
+  0 1 0;
+  0 0 1];
+R = 0.1;
+K_lqr = lqr(A, B, Q, R);
+integrator_initial_condition = [0 omega_R0 0]; % [iq, omega, theta] 
 
-K_lqr = lqr(A,B,eye(3),1);
+open_system('PMSM_generator');                    % open the model
+in = Simulink.SimulationInput('PMSM_generator');  % set simulation parameters
+out = sim(in, 'ShowProgress','on'); 
+
+% sys1 = ss(A-B*K_lqr,B,C,D);
+% step(sys1)
